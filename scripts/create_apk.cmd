9cd97f2837cf99030104ebef6f2b877348c10d78
Tzachi Dar <tzachi.dar@gmail.com>
2017-11-17 00:06:50 +0200

    fix a typo.

--- scripts/create_apk.cmd
+++ scripts/create_apk.cmd
@@ -8,7 +8,7 @@ if exist LibreLink\LibreLink_v1.3.2.4_apkpure.com.apk goto :apk_exists
 tools\windows\wget -O LibreLink\LibreLink_v1.3.2.4_apkpure.com.apk --no-check-certificate "https://download.apkpure.com/b/apk/Y29tLmxpYnJlbGluay5hcHBfMTA2NTdfNzlkZGIyZDc?_fn=TGlicmVMaW5rX3YxLjMuMi40X2Fwa3B1cmUuY29tLmFwaw%3D%3D&k=322c052d0f9c34b28c1f67bc88de53c15a0fe122&as=2f14426570424edf58d775b630faf2645a0d3e9a&_p=Y29tLmxpYnJlbGluay5hcHA%3D&c=1%7CMEDICAL"
 
 mkdir LibreLink\apk
-7z -oLibreLink\apk x LibreLink\LibreLink_v1.3.2.4_apkpure.com.apk
+tools\windows\7z -oLibreLink\apk x LibreLink\LibreLink_v1.3.2.4_apkpure.com.apk
 
 
 :apk_exists
@@ -23,7 +23,7 @@ copy apk\app-debug.apk temp
 rmdir /Q /s temp\dir
 md temp\dir
 
-7z x -otemp\dir temp\app-debug.apk
+tools\windows\7z x -otemp\dir temp\app-debug.apk
 rmdir /Q /s temp\dir\META-INF
 
 copy LibreLink\apk\lib\arm64-v8a\libDataProcessing.so temp\dir\lib\arm64-v8a\libDataProcessing.so
@@ -37,7 +37,7 @@ copy LibreLink\apk\lib\x86_64\libDataProcessing.so temp\dir\lib\x86_64\libDataPr
 copy LibreLink\LibreLink_v1.3.2.4_apkpure.com.apk temp\dir\res\raw\original_apk
 
 cd temp\dir
-7z -tzip a apk.aunaligned.zip
+..\..\tools\windows\7z -tzip a apk.aunaligned.zip
 cd ..\..
 move temp\dir\apk.aunaligned.zip temp
 

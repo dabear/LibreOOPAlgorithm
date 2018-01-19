07de0c6d1244870464a1528eaa05f6cfbb527da7
Tzachi Dar <tzachi.dar@gmail.com>
2018-01-19 09:48:30 +0200

    Print the glucose level to a text file.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- python/xDripDbToRawData.py
+++ python/xDripDbToRawData.py
@@ -3,9 +3,92 @@ import sqlite3
 import sys
 
 
+
+#based on 
+#    https://github.com/keencave/LBridge/blob/master/LBridge_RFduino_170729_1647.ino, line 2164.
+#    https://github.com/UPetersen/LibreMonitor/wiki
+
+
 def WriteBinFile(file_name, bin_data):
     newFile = open(file_name, "wb")
     newFile.write(bin_data)    
+    
+
+def getGlucose(byte0, byte1):
+    return ((256 * (byte0 & 0xFF) + (byte1 & 0xFF)) & 0x0FFF) / 10
+    
+def PrintBytesAsHex(bytearray, text_file):
+    i = 0
+    for byte in bytearray:
+        # print leading line number
+        if not i % 8:
+            print( format(i, '0002x'), ': ',end='', sep='', file=text_file)
+        print(format(byte, '02x'), end='', file=text_file)
+        i+=1
+        # move line or space as needed
+        if i % 8:
+            print(' ',end='', file=text_file)
+        else:
+            print('', file=text_file)
+    if i % 8:
+        print('', file=text_file)
+
+    
+def WriteTxtFile(text_file, data, watchTime):
+
+    indexTrend = data[26] & 0xFF;
+
+    indexHistory = data[27] & 0xFF;
+
+    sensorTime = 256 * (data[317] & 0xFF) + (data[316] & 0xFF);
+    MINUTE = 60
+
+    sensorStartTime = watchTime - sensorTime * MINUTE;
+    print('Header of sensor:', file=text_file)
+    PrintBytesAsHex(data[0:24], text_file)
+
+    
+    # loads trend values (ring buffer, starting at index_trent. byte 28-123)
+    print ('per minute data', file=text_file)
+    for index in range (0, 16):
+        i = indexTrend - index - 1;
+        if (i < 0): i += 16;
+        print('index', i, file=text_file )
+        PrintBytesAsHex(data[i * 6 + 28 : (i + 1) * 6 + 28], text_file)
+        
+        glucoseLevel = getGlucose(data[(i * 6 + 29)], data[(i * 6 + 28)]);
+        time = max(0, sensorTime - index);
+        print ('glucoseLevel = ', glucoseLevel, 'time = ', time, file=text_file)
+
+        
+
+        #        glucoseData.realDate = sensorStartTime + time * MINUTE;
+        #        glucoseData.sensorTime = time;
+
+    
+    # loads history values (ring buffer, starting at index_trent. byte 124-315)
+    print ('Historical data', file=text_file)
+    for index in range (0, 32):
+        i = indexHistory - index - 1;
+        if (i < 0): i += 32;
+        #GlucoseData glucoseData = new GlucoseData();
+        glucoseLevel = getGlucose(data[(i * 6 + 125)], data[i * 6 + 124]);
+        
+        print('index', i, file=text_file )
+        PrintBytesAsHex(data[i * 6 + 28 : (i + 1) * 6 + 28], text_file)
+
+        time = max(0, abs((sensorTime - 3) / 15) * 15 - index * 15);
+        print ('glucoseLevel = ', glucoseLevel, 'time = ', time, file=text_file)
+
+        # glucoseData.realDate = sensorStartTime + time * MINUTE;
+        # glucoseData.sensorId = tagId;
+        # glucoseData.sensorTime = time;
+        # historyList.add(glucoseData);
+
+    print('Footer of sensor:', file=text_file)
+    PrintBytesAsHex(data[320:344], text_file)
+
+
 
 def ReadxDripDb(file_name):
     # gets the latest n non commited objects
@@ -27,6 +110,10 @@ def ReadxDripDb(file_name):
         print(time_string)
         
         WriteBinFile(time_string + '.bin', raw['blockbytes'])
+        
+        file_name = time_string + '.txt'
+        with open(file_name, "w") as text_file:
+            WriteTxtFile(text_file, raw['blockbytes'], raw['timestamp'])
 
     return ret
     
